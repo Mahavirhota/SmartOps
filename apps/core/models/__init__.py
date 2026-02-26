978d827429ee4a75dcccdc0119927e881ded9db0
Mahavir Prasad Hota <37869969+Mahavirhota@users.noreply.github.com>
2026-02-26 12:41:55 +0530

    Fix import sorting in apps/core/models/__init__.py and ignore unused TimeStampedModel

--- apps/core/models/__init__.py
+++ apps/core/models/__init__.py
@@ -1,3 +1,3 @@
-from apps.core.models.base import (TenantAwareModel,  # noqa: F401
-                                   TimeStampedModel)
+from apps.core.models.base import TenantAwareModel  # noqa: F401
+from apps.core.models.base import TimeStampedModel  # noqa: F401
 from apps.core.models.managers import TenantManager  # noqa: F401
